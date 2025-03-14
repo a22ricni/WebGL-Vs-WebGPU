431dc078e91398a51e73fab54275a3316236c64d
a22ricni <a22ricni@student.his.se>
2025-03-14 15:22:27 +0100

    Made the first test scene
    Made the first test scene for three.js and the scene is blank.

---
+++ main.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.inneWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.inneWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+renderer.render(scene, camera);
