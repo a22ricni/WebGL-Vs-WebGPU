0f81e9853eca71845f74a133c6b88fc36bd150b5
a22ricni <a22ricni@student.his.se>
2025-03-18 13:28:57 +0100

    WebGPU is working
    Made it so the WebGPU API is working with three.js.

---
+++ Three-WebGPU.js
@@ -0,0 +1,37 @@
+import * as THREE from "three/webgpu";
+
+const renderer = new THREE.WebGPURenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+
+const gridHelper = new THREE.GridHelper(15, 50);
+scene.add(gridHelper);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(geometry, material);
+
+scene.add(box);
+
+function animate(time){
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
