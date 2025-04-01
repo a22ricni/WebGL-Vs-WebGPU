5b3ff6f7b3fdf1448971b2e7e6476228d0c0c16f
a22ricni <a22ricni@student.his.se>
2025-04-01 14:06:21 +0200

    Added the same thing for the WebGPU on babylon
    Added the same content as the WebGL babylon file but changed so WebGPU API is used.

---
+++ Babylon-WebGPU.js
@@ -0,0 +1,55 @@
+import * as BABYLON from "babylonjs";
+
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.WebGPUEngine(canvas);
+await engine.initAsync();
+
+const createScene = function () {
+    const scene = new BABYLON.Scene(engine);
+
+    //scene.createDefaultCameraOrLight(true, false, true);
+    scene.createDefaultLight();
+
+    const camera = new BABYLON.UniversalCamera(
+        "uCamera",
+        new BABYLON.Vector3(0, 1, -5),
+        scene
+    );
+
+    const box = BABYLON.MeshBuilder.CreateBox(
+        "myBox",
+        {
+            size: 0.1,
+        }
+    )
+
+    const ground = BABYLON.MeshBuilder.CreateGround(
+        "myGround",
+        {
+            width: 3,
+            height: 3,
+            subdivisionsX: 10,
+            subdivisionsY: 20,
+        }
+    );
+
+    scene.registerBeforeRender(function() {
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.01;
+    });
+
+    ground.material = new BABYLON.StandardMaterial();
+    ground.material.wireframe = true;
+
+    return scene;
+};
+
+const scene = createScene();
+
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+window.addEventListener("resize", function () {
+    engine.resize();
+});
