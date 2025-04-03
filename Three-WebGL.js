417acde0d97d86e8e6a5191cf7b372eb8ce1d1ea
a22ricni <a22ricni@student.his.se>
2025-04-03 13:26:42 +0200

    Can render 10000 cubes
    Can render 10000 cubes which have random position.

--- Three-WebGL.js
+++ Three-WebGL.js
@@ -11,13 +11,13 @@ const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    10000
 );
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(0, 2, 1000);
+camera.position.set(0, 2, 1450);
 
 const light = new THREE.HemisphereLight(0xffffff, 0x000000, 4);
 scene.add(light);
@@ -27,14 +27,14 @@ scene.add(gridHelper);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-const mesh = new THREE.InstancedMesh(geometry, material, 1000);
+const mesh = new THREE.InstancedMesh(geometry, material, 10000);
 scene.add(mesh);
 
 const meshPosition = new THREE.Object3D();
-for (let i = 0; i < 1000; i++) {
-    meshPosition.position.x = Math.random() * 2000 - 1000;
-    meshPosition.position.y = 1;
-    meshPosition.position.z = 1;
+for (let i = 0; i < 10000; i++) {
+    meshPosition.position.x = Math.random() * 1000 - 500;
+    meshPosition.position.y = Math.random() * 1000 - 500;
+    meshPosition.position.z = Math.random() * 1500 - 750;
 
     meshPosition.updateMatrix();
     mesh.setMatrixAt(i, meshPosition.matrix);
