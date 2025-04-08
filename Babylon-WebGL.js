18a2e8345854078777c99995e1a103713da376c3
a22ricni <a22ricni@student.his.se>
2025-04-08 12:12:17 +0200

    Implemented thinInstanceSetBuffer for rendering cubes
    Used thinInstanceSetBuffer for rendering a cube.

--- Babylon-WebGL.js
+++ Babylon-WebGL.js
@@ -5,6 +5,7 @@ const engine = new BABYLON.Engine(canvas);
 
 const createScene = function () {
     const scene = new BABYLON.Scene(engine);
+    let amount = 1
 
     //scene.createDefaultCameraOrLight(true, false, true);
     scene.createDefaultLight();
@@ -15,30 +16,36 @@ const createScene = function () {
         scene
     );
 
-    const box = BABYLON.MeshBuilder.CreateBox(
-        "myBox",
-        {
-            size: 0.1,
-        }
-    )
+    const box = BABYLON.MeshBuilder.CreateBox("myBox", {
+        size: 0.1,
+    });
 
-    const ground = BABYLON.MeshBuilder.CreateGround(
-        "myGround",
-        {
-            width: 3,
-            height: 3,
-            subdivisionsX: 10,
-            subdivisionsY: 20,
-        }
-    );
+    let matricesData = new Float32Array(16 * amount);
 
-    scene.registerBeforeRender(function() {
+    for (var i = 0; i < amount; i++) {
+        var matrix = BABYLON.Matrix.Translation(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1)
+        matrix.copyToArray(matricesData, i * 16);
+           
+    }
+
+    box.thinInstanceSetBuffer("matrix", matricesData, 16);
+
+    const ground = BABYLON.MeshBuilder.CreateGround("myGround", {
+        width: 3,
+        height: 3,
+        subdivisionsX: 10,
+        subdivisionsY: 20,
+    });
+
+    scene.registerBeforeRender(function () {
         box.rotation.x += 0.01;
         box.rotation.y += 0.01;
     });
 
     ground.material = new BABYLON.StandardMaterial();
     ground.material.wireframe = true;
+
+    //box.thinInstanceSetBuffer(BABYLON.Matrix.Translation(Math.random() * 2 - 1, 2, 0), amount, 16)
 
     return scene;
 };
