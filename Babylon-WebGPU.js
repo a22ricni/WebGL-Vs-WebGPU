d3bd1acf2446c343159735ef1ca0fa179c882646
a22ricni <a22ricni@student.his.se>
2025-04-14 12:16:29 +0200

    The Babylon WebGPU version is working
    The Babylon WebGPU version is working like the WebGL version.

--- Babylon-WebGPU.js
+++ Babylon-WebGPU.js
@@ -4,42 +4,49 @@ const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.WebGPUEngine(canvas);
 await engine.initAsync();
 
-const createScene = function () {
+function createScene () {
     const scene = new BABYLON.Scene(engine);
+    let amount = 6
 
     //scene.createDefaultCameraOrLight(true, false, true);
     scene.createDefaultLight();
 
     const camera = new BABYLON.UniversalCamera(
         "uCamera",
-        new BABYLON.Vector3(0, 1, -5),
+        new BABYLON.Vector3(0, 1, -50),
         scene
     );
 
-    const box = BABYLON.MeshBuilder.CreateBox(
-        "myBox",
-        {
-            size: 0.1,
-        }
-    )
+    const box = BABYLON.MeshBuilder.CreateBox("myBox", {
+        size: 0.1,
+    });
 
-    const ground = BABYLON.MeshBuilder.CreateGround(
-        "myGround",
-        {
-            width: 3,
-            height: 3,
-            subdivisionsX: 10,
-            subdivisionsY: 20,
-        }
-    );
+    let matricesData = new Float32Array(16 * amount);
 
-    scene.registerBeforeRender(function() {
+    for (var i = 0; i < amount; i++) {
+        var matrix = BABYLON.Matrix.Translation(Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10)
+        matrix.copyToArray(matricesData, i * 16);
+           
+    }
+
+    box.thinInstanceSetBuffer("matrix", matricesData, 16);
+
+    const ground = BABYLON.MeshBuilder.CreateGround("myGround", {
+        width: 3,
+        height: 3,
+        subdivisionsX: 10,
+        subdivisionsY: 20,
+    });
+
+    scene.registerBeforeRender(function () {
         box.rotation.x += 0.01;
         box.rotation.y += 0.01;
     });
 
     ground.material = new BABYLON.StandardMaterial();
     ground.material.wireframe = true;
+
+    //box.thinInstanceSetBuffer(BABYLON.Matrix.Translation(Math.random() * 2 - 1, 2, 0), amount, 16)
 
     return scene;
 };
