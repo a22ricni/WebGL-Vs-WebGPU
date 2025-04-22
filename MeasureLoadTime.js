4046d88640f5cc07f6d0ab3cec0f2fb5521b3323
a22ricni <a22ricni@student.his.se>
2025-04-22 22:39:37 +0200

    Made a load time checker
    Made a load time checker for checking the time for all the cubes to be loading in the scene.

---
+++ MeasureLoadTime.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Measure Loading Time
+// @namespace    Loading Time
+// @version      2025-04-18
+// @description  Measureing the loading time
+// @author       Me
+// @include      http://localhost:5173/*
+// @grant        none
+
+// ==/UserScript==
+
+(function () {
+    //let loadTimeData = []
+
+    const iterations = 5
+    let count = 0
+
+    function beginToMeasure(){
+        let startTime = performance.now()
+        count = parseInt(localStorage.getItem("Counter")) || 0
+        window.addEventListener("allCubesIsLoaded", function() {
+            let endTime = performance.now()
+            let saveData = endTime - startTime
+            count++
+            saveTime(saveData)
+        })
+    }
+
+    function saveTime(saveData) {
+        let loadTimeData = localStorage.getItem("LoadTimeData") || ""
+        loadTimeData += saveData + "\n"
+        localStorage.setItem("LoadTimeData", loadTimeData)
+        localStorage.setItem("Counter", count)
+        if (count >= iterations) {
+            alert("Finished!")
+            download()
+            localStorage.removeItem("LoadTimeData")
+            localStorage.removeItem("Counter")
+            localStorage.clear()
+            return;
+        } else {
+            setTimeout(() => {
+                location.reload()
+            }, 4000)
+        }
+    }
+
+    function download() {
+        //console.log(localStorage.getItem("LoadTimeData"))
+        let anchor = document.createElement("a")
+        let fileName = `LoadTime_Three_WebGL.csv`
+        //let fileName = `LoadTime_Three_WebGPU.csv`
+        //let fileName = `LoadTime_Babylon_WebGL.csv`
+        //let fileName = `LoadTime_Babylon_WebGPU.csv`
+        anchor.setAttribute(
+            "href",
+            "data:text/plain;charset=utf-8," + encodeURI(localStorage.getItem("LoadTimeData"))
+        );
+        anchor.setAttribute("download", fileName)
+        document.body.appendChild(anchor)
+        anchor.click()
+    }
+
+    beginToMeasure()
+
+})();
